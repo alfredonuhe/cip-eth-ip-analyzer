2a30521b6c03f0398e4cac32230d81fffa0f9aa8
alfredonuhe <alfredonuhe@gmail.com>
2019-06-14 11:52:38 -0400

    Added progress display code.

---
+++ progressBar.py
@@ -0,0 +1,23 @@
+"""
+Call in a loop to create terminal progress bar
+@params:
+    iteration   - Required  : current iteration (Int)
+    total       - Required  : total iterations (Int)
+    prefix      - Optional  : prefix string (Str)
+    suffix      - Optional  : suffix string (Str)
+    decimals    - Optional  : positive number of decimals in percent complete (Int)
+    length      - Optional  : character length of bar (Int)
+    fill        - Optional  : bar fill character, must be different than "-" (Str)
+
+Note: 	if the length of the progress bar is too big to fit in a single console line,
+		then the function will not work properly.
+"""
+
+def printProgressBar (iteration, total, prefix = 'Progress', suffix = 'Complete', decimals = 1, length = 50, fill = '█'):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total: 
+        print("\n")
